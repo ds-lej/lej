40f8b9ee38bc52eabd11e8607c89ff9babc4c1a4
Diamond <me@diamondsystems>
2019-03-24 13:52:21 +0500

    added static Nunito fonts

--- resources/views/index.php
+++ resources/views/index.php
@@ -8,9 +8,6 @@
 
     <title><?= config('app.name') ?></title>
 
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
     <link href="<?= asset('css/app.css') ?>" rel="stylesheet">
     <link href="<?= asset('css/theme-classic.css') ?>" rel="stylesheet">
 
